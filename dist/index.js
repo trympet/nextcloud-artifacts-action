e1264cbb56e4e650cf79d45472a3762ec25d9aa6
Trym Lund Flogard <trym@flogard.no>
2021-06-08 17:11:30 +0200

    package and build

--- dist/index.js
+++ dist/index.js
@@ -486,7 +486,8 @@ class NextcloudClient {
         this.headers = { Authorization: 'Basic ' + Buffer.from(`${this.username}:${this.password}`).toString('base64') };
         this.davClient = webdav.createClient(`${this.endpoint.href}remote.php/dav/files/${this.username}`, {
             username: this.username,
-            password: this.password
+            password: this.password,
+            maxBodyLength: 1024 ** 3
         });
     }
     async uploadFiles(files) {
